55ad40b567c3279f4e0011c276368de4b665d04d
Fransiskus Wijaya <150607412+FransiskusWijaya@users.noreply.github.com>
2023-12-03 16:19:12 +0700

    Update splash_screen.dart

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -3,3 +3,12 @@ import 'package:flutter/material.dart';
 import '../../constants.dart';
 import '../sign_in/sign_in_screen.dart';
 import 'components/splash_content.dart';
+
+class SplashScreen extends StatefulWidget {
+  static String routeName = "/splash";
+
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
