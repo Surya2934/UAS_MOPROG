42a629b2be296b35f637b7081cf9638919fe8a43
ferrary-wj <150898334+ferrary-wj@users.noreply.github.com>
2023-12-03 18:02:04 +0700

    Update cart_card.dart

--- lib/screens/cart/components/cart_card.dart
+++ lib/screens/cart/components/cart_card.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 import '../../../constants.dart';
 import '../../../models/Cart.dart';
+
+class CartCard extends StatelessWidget {
+  const CartCard({
+    Key? key,
+    required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
