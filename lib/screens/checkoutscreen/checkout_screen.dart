635b69c37fd254cf253a1125c774f8ba717c6bd5
ferrary-wj <150898334+ferrary-wj@users.noreply.github.com>
2023-12-03 18:09:18 +0700

    Update checkout_screen.dart

--- lib/screens/checkoutscreen/checkout_screen.dart
+++ lib/screens/checkoutscreen/checkout_screen.dart
@@ -11,3 +11,65 @@ class CheckoutScreen extends StatefulWidget {
 class _CheckoutScreenState extends State<CheckoutScreen> {
   String selectedExpedition = 'Pengiriman Standar';
   String selectedPaymentMethod = 'Kartu Kredit';
+  
+late Product cart;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Checkout'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Informasi Pengiriman',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Nama Lengkap'),
+              ),
+              const SizedBox(height: 8),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Nomor Handphone'),
+              ),
+              const SizedBox(height: 8),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Alamat'),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Ekspedisi',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              DropdownButton<String>(
+                value: selectedExpedition,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedExpedition = newValue!;
+                  });
+                },
+                items: <String>[
+                  'Pengiriman Standar',
+                  'Pengiriman Cepat Gratis Ongkir',
+                  'Pengiriman LEBAY'
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const Text(
+                'Metode Pembayaran',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
