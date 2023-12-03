164b4f299e9b9b21b731de621d21fe71a4ca9ba9
Givvven <150896315+Givvven@users.noreply.github.com>
2023-12-03 17:07:38 +0700

    Create forgot_pass_form.dart

---
+++ lib/screens/forgot_password/components/forgot_pass_form.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import '../../../components/custom_surfix_icon.dart';
+import '../../../components/form_error.dart';
+import '../../../components/no_account_text.dart';
+import '../../../constants.dart';
+import '../../otp/otp_screen.dart';
