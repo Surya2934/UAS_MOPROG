58e445b703389912d8f869058cd6a749625c0221
Fransiskus Wijaya <150607412+FransiskusWijaya@users.noreply.github.com>
2023-12-03 16:25:06 +0700

    Update init_screen.dart

--- lib/screens/init_screen.dart
+++ lib/screens/init_screen.dart
@@ -5,9 +5,7 @@ import 'package:shop_app/screens/cart/cart_screen.dart';
 import 'package:shop_app/screens/favorite/favorite_screen.dart';
 import 'package:shop_app/screens/home/home_screen.dart';
 
-
 const Color inActiveIconColor = Color(0xFFB6B6B6);
-
 
 class InitScreen extends StatefulWidget {
   const InitScreen({super.key});
