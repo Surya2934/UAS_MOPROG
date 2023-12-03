6e7a5ca44816bcfa0415ad627ebb773f538d3dbc
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:49:36 +0700

    Create section_tittle.dart

---
+++ lib/screens/home/components/section_tittle.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SectionTitle extends StatelessWidget {
+  const SectionTitle({
+    Key? key,
+    required this.title,
+    required this.press,
+  }) : super(key: key);
