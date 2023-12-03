5797704252d06de340e366eb99eceaa2cfb392dc
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:48:38 +0700

    Update search_field.dart

--- lib/screens/home/components/search_field.dart
+++ lib/screens/home/components/search_field.dart
@@ -5,3 +5,24 @@ class SearchField extends StatelessWidget {
   const SearchField({
     Key? key,
   }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: TextFormField(
+        onChanged: (value) {},
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: kSecondaryColor.withOpacity(0.1),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          border: searchOutlineInputBorder,
+          focusedBorder: searchOutlineInputBorder,
+          enabledBorder: searchOutlineInputBorder,
+          hintText: "Cari Produk",
+          prefixIcon: const Icon(Icons.search),
+        ),
+      ),
+    );
+  }
+}
