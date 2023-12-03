bcd0609a902d9efcc8b98f2edfd996a9ca29b820
Fransiskus Wijaya <150607412+FransiskusWijaya@users.noreply.github.com>
2023-12-03 16:11:38 +0700

    Create splash_content.dart

---
+++ lib/screens/splash/components/splash_content.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class SplashContent extends StatefulWidget {
+  const SplashContent({
+    Key? key,
+    this.text,
+    this.image,
+  }) : super(key: key);
+  final String? text, image;
+
+  @override
+  State<SplashContent> createState() => _SplashContentState();
+}
