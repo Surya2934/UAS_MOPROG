3a7f082dac53f61aac7e367b82ba54d33c66e1c8
Surya2934 <150609592+Surya2934@users.noreply.github.com>
2023-12-03 16:03:36 +0700

    Create products_screen.dart

---
+++ lib/screens/details/products/products_screen.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/components/product_card.dart';
+import 'package:shop_app/models/Product.dart';
+
+import '../details/details_screen.dart';
