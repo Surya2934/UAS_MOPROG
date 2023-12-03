e378f45eaddb1668002fb099994017f84eec691d
ferrary-wj <150898334+ferrary-wj@users.noreply.github.com>
2023-12-03 18:05:25 +0700

    Update check_out_card.dart

--- lib/screens/cart/components/check_out_card.dart
+++ lib/screens/cart/components/check_out_card.dart
@@ -2,3 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shop_app/screens/checkoutscreen/checkout_screen.dart';
 import '../../../constants.dart';
+
+class CheckoutCard extends StatelessWidget {
+  const CheckoutCard({
+    Key? key,
+  }) : super(key: key);
