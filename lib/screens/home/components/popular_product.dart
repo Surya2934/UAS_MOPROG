9ac3ad1f9a0f8ebec61a307d324cb86d1eeea56a
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:43:46 +0700

    Update popular_product.dart

--- lib/screens/home/components/popular_product.dart
+++ lib/screens/home/components/popular_product.dart
@@ -4,3 +4,6 @@ import '../../../models/Product.dart';
 import '../../details/details_screen.dart';
 import '../../products/products_screen.dart';
 import 'section_title.dart';
+
+class PopularProducts extends StatelessWidget {
+  const PopularProducts({super.key});
