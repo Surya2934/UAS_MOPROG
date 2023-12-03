4220f93b44a245714250febef3e75f78a4108f2b
Givvven <150896315+Givvven@users.noreply.github.com>
2023-12-03 17:18:42 +0700

    Create otp_form.dart

---
+++ lib/screens/otp/components/otp_form.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/init_screen.dart';
+
+import '../../../constants.dart';
+
+class OtpForm extends StatefulWidget {
+  const OtpForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _OtpFormState createState() => _OtpFormState();
+}
