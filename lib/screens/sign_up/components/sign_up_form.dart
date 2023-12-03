20b749bbba33cf1d9ef6bf6110a188d156097091
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:56:13 +0700

    Update sign_up_form.dart

--- lib/screens/sign_up/components/sign_up_form.dart
+++ lib/screens/sign_up/components/sign_up_form.dart
@@ -3,3 +3,6 @@ import '../../../components/custom_surfix_icon.dart';
 import '../../../components/form_error.dart';
 import '../../../constants.dart';
 import '../../complete_profile/complete_profile_screen.dart';
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({super.key});
