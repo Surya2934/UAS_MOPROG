7ce5abc74869dcecfd088fce7dbd6172927fd53e
arsreen <150603995+arsreen@users.noreply.github.com>
2023-12-03 17:43:51 +0700

    Update login_success_screen.dart

--- lib/screens/login_success/login_success_screen.dart
+++ lib/screens/login_success/login_success_screen.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/screens/init_screen.dart';
+
+class LoginSuccessScreen extends StatelessWidget {
+  static String routeName = "/login_success";
+
+  const LoginSuccessScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const SizedBox(),
+        title: const Text("Login Success"),
+      ),
