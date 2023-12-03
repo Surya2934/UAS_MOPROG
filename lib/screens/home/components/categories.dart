e8e6605cb9b3c890b1ff946263a7f886dd8c838b
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:34:38 +0700

    Update categories.dart

--- lib/screens/home/components/categories.dart
+++ lib/screens/home/components/categories.dart
@@ -4,3 +4,13 @@ import 'package:shop_app/screens/home/components/section_title.dart';
 
 class Categories extends StatelessWidget {
   const Categories({super.key});
+  
+@override
+  Widget build(BuildContext context) {
+    List<Map<String, dynamic>> categories = [
+      {"icon": "assets/icons/Flash Icon.svg", "text": "Flash Deal"},
+      {"icon": "assets/icons/Bill Icon.svg", "text": "Bill"},
+      {"icon": "assets/icons/Game Icon.svg", "text": "Game"},
+      {"icon": "assets/icons/Gift Icon.svg", "text": "Daily Gift"},
+      {"icon": "assets/icons/Discover.svg", "text": "More"},
+    ];
