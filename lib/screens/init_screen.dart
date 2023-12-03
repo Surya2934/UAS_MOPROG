e16558ca5afa016ec59253ce4d6211e9aa6af7af
Fransiskus Wijaya <150607412+FransiskusWijaya@users.noreply.github.com>
2023-12-03 16:16:10 +0700

    Update init_screen.dart

--- lib/screens/init_screen.dart
+++ lib/screens/init_screen.dart
@@ -4,3 +4,14 @@ import 'package:shop_app/constants.dart';
 import 'package:shop_app/screens/cart/cart_screen.dart';
 import 'package:shop_app/screens/favorite/favorite_screen.dart';
 import 'package:shop_app/screens/home/home_screen.dart';
+
+const Color inActiveIconColor = Color(0xFFB6B6B6);
+
+class InitScreen extends StatefulWidget {
+  const InitScreen({super.key});
+
+  static String routeName = "/";
+
+  @override
+  State<InitScreen> createState() => _InitScreenState();
+}
