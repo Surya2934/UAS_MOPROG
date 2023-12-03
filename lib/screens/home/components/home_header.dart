a9c5bcea45813ac34e51f721b8564f0b93c0dbee
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:39:55 +0700

    Update home_header.dart

--- lib/screens/home/components/home_header.dart
+++ lib/screens/home/components/home_header.dart
@@ -7,3 +7,24 @@ class HomeHeader extends StatelessWidget {
   const HomeHeader({
     Key? key,
   }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Expanded(child: SearchField()),
+          const SizedBox(width: 16),
+          const SizedBox(width: 8),
+          IconBtnWithCounter(
+            svgSrc: "assets/icons/Bell.svg",
+            numOfitem: 3,
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
