5b290d012c52c2656d5367165ccba8976bbdb4b8
arsreen <150603995+arsreen@users.noreply.github.com>
2023-12-03 18:44:56 +0700

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -24,3 +24,18 @@ class AppTheme {
         focusedBorder: outlineInputBorder,
         border: outlineInputBorder,
       ),
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          backgroundColor: kPrimaryColor,
+          foregroundColor: Colors.white,
+          minimumSize: const Size(double.infinity, 48),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+        ),
+      ),
+    );
+  }
+}
