d43f93e3c75d42dde00b8f7f82ceab490bd7147c
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:41:16 +0700

    Update icon_btn_with_counter.dart

--- lib/screens/home/components/icon_btn_with_counter.dart
+++ lib/screens/home/components/icon_btn_with_counter.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../../../constants.dart';
+
+class IconBtnWithCounter extends StatelessWidget {
+  const IconBtnWithCounter({
+    Key? key,
+    required this.svgSrc,
+    this.numOfitem = 0,
+    required this.press,
+  }) : super(key: key);
