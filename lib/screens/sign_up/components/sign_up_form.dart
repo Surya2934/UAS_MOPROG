15f3e16d454917396ed9314a54e526e4faed17db
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 20:56:01 +0700

    Update sign_up_form.dart

--- lib/screens/sign_up/components/sign_up_form.dart
+++ lib/screens/sign_up/components/sign_up_form.dart
@@ -139,7 +139,7 @@ class _SignUpFormState extends State<SignUpForm> {
               if (_formKey.currentState!.validate()) {
                 _formKey.currentState!.save();
                 // if all are valid then go to success screen
-                Navigator.pushNamed(context, CompleteProfileScreen.routeName);
+                Navigator.pushNamed(context, HomeScreen.routeName);
               }
             },
             child: const Text("Lanjutkan"),
