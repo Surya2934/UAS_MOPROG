d09fd6456b92e4e833bdf6f1c6da4aecdcb05750
Surya2934 <150609592+Surya2934@users.noreply.github.com>
2023-12-03 18:17:18 +0700

    Update product_description.dart

--- lib/screens/details/components/product_description.dart
+++ lib/screens/details/components/product_description.dart
@@ -3,3 +3,13 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../constants.dart';
 import '../../../models/Product.dart';
+
+class ProductDescription extends StatelessWidget {
+  const ProductDescription({
+    Key? key,
+    required this.product,
+    this.pressOnSeeMore,
+  }) : super(key: key);
+
+  final Product product;
+  final GestureTapCallback? pressOnSeeMore;
