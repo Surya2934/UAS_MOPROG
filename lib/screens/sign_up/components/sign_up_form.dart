296bf90723b32c3bca4453f824ae6c366a294ac3
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:55:43 +0700

    Create sign_up_form.dart

---
+++ lib/screens/sign_up/components/sign_up_form.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import '../../../components/custom_surfix_icon.dart';
+import '../../../components/form_error.dart';
+import '../../../constants.dart';
+import '../../complete_profile/complete_profile_screen.dart';
