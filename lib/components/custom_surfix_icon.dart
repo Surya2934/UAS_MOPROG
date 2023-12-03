ed40aaca49f2e9b7d49a9ad72298f8f6240cebc8
Surya2934 <150609592+Surya2934@users.noreply.github.com>
2023-12-03 19:00:57 +0700

    Update custom_surfix_icon.dart

--- lib/components/custom_surfix_icon.dart
+++ lib/components/custom_surfix_icon.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomSurffixIcon extends StatelessWidget {
+  const CustomSurffixIcon({
+    Key? key,
+    required this.svgIcon,
+  }) : super(key: key);
+
+  final String svgIcon;
