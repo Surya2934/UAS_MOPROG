186fa94f5e82763b1f75ee83085dd555e02fa834
Fransiskus Wijaya <150607412+FransiskusWijaya@users.noreply.github.com>
2023-12-03 16:19:33 +0700

    Update splash_screen.dart

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -12,3 +12,21 @@ class SplashScreen extends StatefulWidget {
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
+
+class _SplashScreenState extends State<SplashScreen> {
+  int currentPage = 0;
+  List<Map<String, String>> splashData = [
+    {
+      "text": "Selamat Datang di Lebay! \nDimana semua harga sangat LEBAY",
+      "image": "assets/images/Gambar 1.png"
+    },
+    {
+      "text":
+          "Kamu Lebay? Dia Lebay? Semua Lebay! \nAYO LEBAY BERSAMA!!",
+      "image": "assets/images/Gambar 2.png"
+    },
+    {
+      "text": "Dengan berbelanja di LEBAY. \nUang-mu akan habis dengan ELEGAN!!",
+      "image": "assets/images/Gambar 3.png"
+    },
+  ];
