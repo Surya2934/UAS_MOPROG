d8afc3c2217e5b501442877341d041cc3958b4af
Surya2934 <150609592+Surya2934@users.noreply.github.com>
2023-12-03 21:08:23 +0700

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,7 +4,6 @@ import 'package:shop_app/screens/checkoutscreen/checkout_screen.dart';
 import 'package:shop_app/screens/products/products_screen.dart';
 
 import 'screens/cart/cart_screen.dart';
-import 'screens/complete_profile/complete_profile_screen.dart';
 import 'screens/details/details_screen.dart';
 import 'screens/forgot_password/forgot_password_screen.dart';
 import 'screens/home/home_screen.dart';
