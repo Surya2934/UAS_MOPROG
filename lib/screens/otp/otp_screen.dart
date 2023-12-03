c5271b22a3912832d5ccede07dc9993da8d9faea
Givvven <150896315+Givvven@users.noreply.github.com>
2023-12-03 17:27:24 +0700

    Update otp_screen.dart

--- lib/screens/otp/otp_screen.dart
+++ lib/screens/otp/otp_screen.dart
@@ -3,3 +3,14 @@ import 'package:flutter/material.dart';
 import '../../constants.dart';
 
 import 'components/otp_form.dart';
+
+class OtpScreen extends StatelessWidget {
+  static String routeName = "/otp";
+
+  const OtpScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Verifikasi OTP"),
+      ),
