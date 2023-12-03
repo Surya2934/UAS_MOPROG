711be51c72fdbcc257ab69022327d81e48d770e4
ferrary-wj <150898334+ferrary-wj@users.noreply.github.com>
2023-12-03 18:03:02 +0700

    Update cart_card.dart

--- lib/screens/cart/components/cart_card.dart
+++ lib/screens/cart/components/cart_card.dart
@@ -9,3 +9,22 @@ class CartCard extends StatelessWidget {
   }) : super(key: key);
 
   final Cart cart;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 88,
+          child: AspectRatio(
+            aspectRatio: 0.88,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: const Color(0xFFF5F6F9),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Image.asset(cart.product.images[0]),
+            ),
+          ),
+        ),
