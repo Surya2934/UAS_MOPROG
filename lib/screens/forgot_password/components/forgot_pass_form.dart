cae4298ab2ba01f39abb14c9c5393e1beac63617
Givvven <150896315+Givvven@users.noreply.github.com>
2023-12-03 17:09:54 +0700

    Update forgot_pass_form.dart

--- lib/screens/forgot_password/components/forgot_pass_form.dart
+++ lib/screens/forgot_password/components/forgot_pass_form.dart
@@ -5,3 +5,11 @@ import '../../../components/form_error.dart';
 import '../../../components/no_account_text.dart';
 import '../../../constants.dart';
 import '../../otp/otp_screen.dart';
+
+
+class ForgotPassForm extends StatefulWidget {
+  const ForgotPassForm({super.key});
+
+  @override
+  _ForgotPassFormState createState() => _ForgotPassFormState();
+}
