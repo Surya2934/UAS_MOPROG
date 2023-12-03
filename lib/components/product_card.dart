3423693b3440aa5c14ac9150b004e9bb61115db8
Givvven <150896315+Givvven@users.noreply.github.com>
2023-12-03 19:07:13 +0700

    Update product_card.dart

--- lib/components/product_card.dart
+++ lib/components/product_card.dart
@@ -3,3 +3,16 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import '../constants.dart';
 import '../models/Product.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    Key? key,
+    this.width = 140,
+    this.aspectRetio = 1.02,
+    required this.product,
+    required this.onPress,
+  }) : super(key: key);
+
+  final double width, aspectRetio;
+  final Product product;
+  final VoidCallback onPress;
