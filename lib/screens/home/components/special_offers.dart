c513972747d297049a2e83242537fba0d502251c
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:51:37 +0700

    Update special_offers.dart

--- lib/screens/home/components/special_offers.dart
+++ lib/screens/home/components/special_offers.dart
@@ -6,3 +6,15 @@ class SpecialOffers extends StatelessWidget {
   const SpecialOffers({
     Key? key,
   }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SectionTitle(
+            title: "Spesial untuk kamu yang LEBAY",
+            press: () {},
+          ),
+        ),
