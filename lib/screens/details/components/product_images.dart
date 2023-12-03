c0dc5603059a70620bb87675054ca3c46abc287f
Surya2934 <150609592+Surya2934@users.noreply.github.com>
2023-12-03 18:22:30 +0700

    Update product_images.dart

--- lib/screens/details/components/product_images.dart
+++ lib/screens/details/components/product_images.dart
@@ -65,3 +65,26 @@ class SmallProductImage extends StatefulWidget {
   @override
   State<SmallProductImage> createState() => _SmallProductImageState();
 }
+
+class _SmallProductImageState extends State<SmallProductImage> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.press,
+      child: AnimatedContainer(
+        duration: defaultDuration,
+        margin: const EdgeInsets.only(right: 16),
+        padding: const EdgeInsets.all(8),
+        height: 48,
+        width: 48,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+              color: kPrimaryColor.withOpacity(widget.isSelected ? 1 : 0)),
+        ),
+        child: Image.asset(widget.image),
+      ),
+    );
+  }
+}
