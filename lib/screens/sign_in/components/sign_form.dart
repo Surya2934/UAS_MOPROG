7efbab0d343ebffca6831621b3af0bfd2b4550ee
arsreen <150603995+arsreen@users.noreply.github.com>
2023-12-03 17:45:19 +0700

    Update sign_form.dart

--- lib/screens/sign_in/components/sign_form.dart
+++ lib/screens/sign_in/components/sign_form.dart
@@ -6,3 +6,10 @@ import '../../../constants.dart';
 import '../../../helper/keyboard.dart';
 import '../../forgot_password/forgot_password_screen.dart';
 import '../../login_success/login_success_screen.dart';
+
+class SignForm extends StatefulWidget {
+  const SignForm({super.key});
+
+  @override
+  _SignFormState createState() => _SignFormState();
+}
