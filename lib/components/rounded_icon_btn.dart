637011be08522f37d778775b32e5b5775dc10166
Fransiskus Wijaya <150607412+FransiskusWijaya@users.noreply.github.com>
2023-12-03 19:12:38 +0700

    Update rounded_icon_btn.dart

--- lib/components/rounded_icon_btn.dart
+++ lib/components/rounded_icon_btn.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 
 import '../constants.dart';
+class RoundedIconBtn extends StatelessWidget {
+  const RoundedIconBtn({
+    Key? key,
+    required this.icon,
+    required this.press,
+    this.showShadow = false,
+  }) : super(key: key);
