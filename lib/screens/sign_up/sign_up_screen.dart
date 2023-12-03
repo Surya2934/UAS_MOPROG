e453ac2dfc56a5336dd1b7983e85005b44ace400
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:59:28 +0700

    Update sign_up_screen.dart

--- lib/screens/sign_up/sign_up_screen.dart
+++ lib/screens/sign_up/sign_up_screen.dart
@@ -5,3 +5,11 @@ import 'components/sign_up_form.dart';
 
 class SignUpScreen extends StatelessWidget {
   static String routeName = "/sign_up";
+
+const SignUpScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sign Up"),
+      ),
