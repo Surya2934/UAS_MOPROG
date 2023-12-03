216465c4fb1c2d3ce9d454536357b5bf3011dc91
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:51:22 +0700

    Update special_offers.dart

--- lib/screens/home/components/special_offers.dart
+++ lib/screens/home/components/special_offers.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/screens/products/products_screen.dart';
 import 'section_title.dart';
+
+class SpecialOffers extends StatelessWidget {
+  const SpecialOffers({
+    Key? key,
+  }) : super(key: key);
