ef89c7d17d0bc94ef7e0649713f9e267ece43b5d
Fransiskus Wijaya <150607412+FransiskusWijaya@users.noreply.github.com>
2023-12-03 19:13:54 +0700

    Update no_account_text.dart

--- lib/components/no_account_text.dart
+++ lib/components/no_account_text.dart
@@ -6,3 +6,23 @@ class NoAccountText extends StatelessWidget {
   const NoAccountText({
     Key? key,
   }) : super(key: key);
+ @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "Don’t have an account? ",
+          style: TextStyle(fontSize: 16),
+        ),
+        GestureDetector(
+          onTap: () => Navigator.pushNamed(context, SignUpScreen.routeName),
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(fontSize: 16, color: kPrimaryColor),
+          ),
+        ),
+      ],
+    );
+  }
+}
