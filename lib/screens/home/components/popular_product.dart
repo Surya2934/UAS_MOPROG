61d329ea9f34e9ae2bb6535935f88519a5620de5
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:43:20 +0700

    Create popular_product.dart

---
+++ lib/screens/home/components/popular_product.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import '../../../components/product_card.dart';
+import '../../../models/Product.dart';
+import '../../details/details_screen.dart';
+import '../../products/products_screen.dart';
+import 'section_title.dart';
