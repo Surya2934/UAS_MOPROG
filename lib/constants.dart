56ed75b8e9334edefea93841cd32f7a8ba15dfc8
Givvven <150896315+Givvven@users.noreply.github.com>
2023-12-03 18:55:20 +0700

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,3 +7,14 @@ const kPrimaryGradientColor = LinearGradient(
   end: Alignment.bottomRight,
   colors: [Color.fromARGB(121, 60, 176, 91), Color.fromARGB(255, 60, 176, 91)],
 );
+const kSecondaryColor = Color(0xFF979797);
+const kTextColor = Colors.black;
+
+const kAnimationDuration = Duration(milliseconds: 200);
+
+const headingStyle = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+  height: 1.5,
+);
