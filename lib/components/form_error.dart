df4d42e5092eaf28c0d4fe6b190ca34c7d8a601c
Givvven <150896315+Givvven@users.noreply.github.com>
2023-12-03 19:13:24 +0700

    Create form_error.dart

---
+++ lib/components/form_error.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class FormError extends StatelessWidget {
+  const FormError({
+    Key? key,
+    required this.errors,
+  }) : super(key: key);
