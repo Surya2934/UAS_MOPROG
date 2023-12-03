5a1cea89e278f1fb9b26e2f86ce04d1ca07306de
Fransiskus Wijaya <150607412+FransiskusWijaya@users.noreply.github.com>
2023-12-03 19:12:15 +0700

    Update no_account_text.dart

--- lib/components/no_account_text.dart
+++ lib/components/no_account_text.dart
@@ -2,3 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../constants.dart';
 import '../screens/sign_up/sign_up_screen.dart';
+class NoAccountText extends StatelessWidget {
+  const NoAccountText({
+    Key? key,
+  }) : super(key: key);
