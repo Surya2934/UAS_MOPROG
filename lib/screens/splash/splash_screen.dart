23bcf29800a71e91b2ef3e62e80501e042175847
Fransiskus Wijaya <150607412+FransiskusWijaya@users.noreply.github.com>
2023-12-03 16:19:56 +0700

    Update splash_screen.dart

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -30,3 +30,74 @@ class _SplashScreenState extends State<SplashScreen> {
       "image": "assets/images/Gambar 3.png"
     },
   ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                flex: 3,
+                child: PageView.builder(
+                  onPageChanged: (value) {
+                    setState(() {
+                      currentPage = value;
+                    });
+                  },
+                  itemCount: splashData.length,
+                  itemBuilder: (context, index) => SplashContent(
+                    image: splashData[index]["image"],
+                    text: splashData[index]['text'],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: <Widget>[
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(
+                          splashData.length,
+                          (index) => AnimatedContainer(
+                            duration: kAnimationDuration,
+                            margin: const EdgeInsets.only(right: 5),
+                            height: 6,
+                            width: currentPage == index ? 20 : 6,
+                            decoration: BoxDecoration(
+                              color: currentPage == index
+                                  ? kPrimaryColor
+                                  : const Color(0xFFD8D8D8),
+                              borderRadius: BorderRadius.circular(3),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Spacer(flex: 3),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, SignInScreen.routeName);
+                        },
+                        child: const Text(
+                          "Lanjutkan",
+                          style: TextStyle(fontSize: 21),
+                        ),
+                      ),
+                      const Spacer(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
