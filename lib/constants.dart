d294da917d66513e8e66119cec9b04b69ab812c8
Givvven <150896315+Givvven@users.noreply.github.com>
2023-12-03 18:54:37 +0700

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 60, 176, 91);
+const kPrimaryLightColor = Color.fromARGB(121, 60, 176, 91);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color.fromARGB(121, 60, 176, 91), Color.fromARGB(255, 60, 176, 91)],
+);
