1b546e25b27aa8ec76ba0ff36ea22a380fd48617
Givvven <150896315+Givvven@users.noreply.github.com>
2023-12-03 19:09:00 +0700

    Update product_card.dart

--- lib/components/product_card.dart
+++ lib/components/product_card.dart
@@ -37,3 +37,20 @@ class ProductCard extends StatelessWidget {
                 child: Image.asset(product.images[0]),
               ),
             ),
+            const SizedBox(height: 8),
+            Text(
+              product.title,
+              style: Theme.of(context).textTheme.bodyMedium,
+              maxLines: 2,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "\Rp.${product.price}",
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: kPrimaryColor,
+                  ),
+                ),
