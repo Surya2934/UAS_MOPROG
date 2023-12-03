ab88ca50ff9f3828eca1fde7cc0e75a740ef6e0a
arsreen <150603995+arsreen@users.noreply.github.com>
2023-12-03 18:42:37 +0700

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -14,3 +14,21 @@ import 'screens/otp/otp_screen.dart';
 import 'screens/sign_in/sign_in_screen.dart';
 import 'screens/sign_up/sign_up_screen.dart';
 import 'screens/splash/splash_screen.dart';
+
+
+
+final Map<String, WidgetBuilder> routes = {
+  InitScreen.routeName: (context) => const InitScreen(),
+  SplashScreen.routeName: (context) => const SplashScreen(), 
+  SignInScreen.routeName: (context) => const SignInScreen(), 
+  ForgotPasswordScreen.routeName: (context) => const ForgotPasswordScreen(),
+  LoginSuccessScreen.routeName: (context) => const LoginSuccessScreen(),
+  SignUpScreen.routeName: (context) => const SignUpScreen(),
+  OtpScreen.routeName: (context) => const OtpScreen(),
+  HomeScreen.routeName: (context) => const HomeScreen(),
+  ProductsScreen.routeName: (context) => const ProductsScreen(),
+  DetailsScreen.routeName: (context) => const DetailsScreen(),
+  CartScreen.routeName: (context) => const CartScreen(),
+  CheckoutScreen.routeName: (context) => CheckoutScreen(),
+  Splash.routeName: (context) => Splash(),
+};
