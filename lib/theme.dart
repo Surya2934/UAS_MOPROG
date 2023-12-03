7bb36d01b16cffba590da49d8540a6f76c5f6a90
arsreen <150603995+arsreen@users.noreply.github.com>
2023-12-03 18:44:27 +0700

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,3 +1,19 @@
 import 'package:flutter/material.dart';
 
 import 'constants.dart';
+
+class AppTheme {
+  static ThemeData lightTheme(BuildContext context) {
+    return ThemeData(
+      scaffoldBackgroundColor: Colors.white,
+      fontFamily: "Muli",
+      appBarTheme: const AppBarTheme(
+          color: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+          titleTextStyle: TextStyle(color: Colors.black)),
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: kTextColor),
+        bodyMedium: TextStyle(color: kTextColor),
+        bodySmall: TextStyle(color: kTextColor),
+      ),
