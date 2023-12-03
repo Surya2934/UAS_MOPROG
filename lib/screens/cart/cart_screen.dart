18c4741eb44f2006a6d36a2a1556681c94e3b538
ferrary-wj <150898334+ferrary-wj@users.noreply.github.com>
2023-12-03 17:59:06 +0700

    Update cart_screen.dart

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -3,3 +3,12 @@ import 'package:flutter_svg/flutter_svg.dart';
 import '../../models/Cart.dart';
 import 'components/cart_card.dart';
 import 'components/check_out_card.dart';
+
+class CartScreen extends StatefulWidget {
+  static String routeName = "/cart";
+
+  const CartScreen({super.key});
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
