b6514b2fde386142ea878be0a4192dcabe4e9417
arsreen <150603995+arsreen@users.noreply.github.com>
2023-12-03 18:39:57 +0700

    Create routes.dart

---
+++ lib/routes.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/widgets.dart';
+import 'package:shop_app/main.dart';
+import 'package:shop_app/screens/checkoutscreen/checkout_screen.dart';
+import 'package:shop_app/screens/products/products_screen.dart';
