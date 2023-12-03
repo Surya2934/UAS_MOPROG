c233428802ef297ad572858345ee54d9e5b68828
ferrary-wj <150898334+ferrary-wj@users.noreply.github.com>
2023-12-03 18:00:03 +0700

    Update cart_screen.dart

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -12,6 +12,7 @@ class CartScreen extends StatefulWidget {
   @override
   State<CartScreen> createState() => _CartScreenState();
 }
+
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
