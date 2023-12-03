bd874f9dc824191ecc8a9228d5050461ae8fa5af
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:37:28 +0700

    Update discount_banner.dart

--- lib/screens/home/components/discount_banner.dart
+++ lib/screens/home/components/discount_banner.dart
@@ -4,3 +4,17 @@ class DiscountBanner extends StatelessWidget {
   const DiscountBanner({
     Key? key,
   }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(20),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 16,
+      ),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 60, 176, 91),
+        borderRadius: BorderRadius.circular(20),
+      ),
