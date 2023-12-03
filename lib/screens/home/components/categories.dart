2f4b07773f8f39568c16211f825051e716cfe8fc
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2023-12-03 16:34:10 +0700

    Update categories.dart

--- lib/screens/home/components/categories.dart
+++ lib/screens/home/components/categories.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shop_app/screens/home/components/section_title.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({super.key});
