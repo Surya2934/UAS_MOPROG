1d727f99efc28c08d6eb70d99885b47bd9223ccb
Surya2934 <150609592+Surya2934@users.noreply.github.com>
2023-12-03 18:23:24 +0700

    Create top_rounded_container.dart

---
+++ lib/screens/details/components/top_rounded_container.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TopRoundedContainer extends StatelessWidget {
+  const TopRoundedContainer({
+    Key? key,
+    required this.color,
+    required this.child,
+  }) : super(key: key);
+
+  final Color color;
+  final Widget child;
