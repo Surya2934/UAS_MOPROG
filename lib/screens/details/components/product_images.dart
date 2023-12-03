acc41837d7b38cb37b26d2f6581252e4f07f1dbe
Surya2934 <150609592+Surya2934@users.noreply.github.com>
2023-12-03 18:20:44 +0700

    Update product_images.dart

--- lib/screens/details/components/product_images.dart
+++ lib/screens/details/components/product_images.dart
@@ -2,3 +2,15 @@ import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
 import '../../../models/Product.dart';
+
+class ProductImages extends StatefulWidget {
+  const ProductImages({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  _ProductImagesState createState() => _ProductImagesState();
+}
