90db2f1c08c8a2a977550bb3d9f2775213f4b055
ferrary-wj <150898334+ferrary-wj@users.noreply.github.com>
2023-12-03 18:08:49 +0700

    Update checkout_screen.dart

--- lib/screens/checkoutscreen/checkout_screen.dart
+++ lib/screens/checkoutscreen/checkout_screen.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/models/Cart.dart';
 import 'package:shop_app/models/Product.dart';
+
+class CheckoutScreen extends StatefulWidget {
+  @override
+  _CheckoutScreenState createState() => _CheckoutScreenState();
+  static String routeName = "/checkout";
+}
+
+class _CheckoutScreenState extends State<CheckoutScreen> {
+  String selectedExpedition = 'Pengiriman Standar';
+  String selectedPaymentMethod = 'Kartu Kredit';
